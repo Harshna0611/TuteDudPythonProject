2f84c44316149211517542926dcfca74250ce319
Harshna <harshncvrce0602@gmail.com>
2025-06-21 19:48:27 +0530

    module1

---
+++ Module2Basic Python Concepts.py
@@ -0,0 +1,13 @@
+
+num1= int(input('Enter the first number: '))
+num2= int(input('\nEnter the second number: '))
+
+sum = num1+num2
+diff = num1-num2
+multiply = num1*num2
+div = num1/num2
+
+print('\n Addition: ',sum)
+print('Subtraction: ',diff)
+print('Multiplication: ',multiply)
+print('Division: ',div)
