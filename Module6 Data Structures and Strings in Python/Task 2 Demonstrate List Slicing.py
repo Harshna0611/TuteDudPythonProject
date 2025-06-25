178d14c4614d15eff451b53e4cde62e7f2b2fa03
Harshna <harshncvrce0602@gmail.com>
2025-06-25 18:27:52 +0530

    module1 update

--- Module6 Data Structures and Strings in Python/Task 2 Demonstrate List Slicing.py
+++ Module6 Data Structures and Strings in Python/Task 2 Demonstrate List Slicing.py
@@ -1,4 +1,5 @@
 num= [i for i in range(1,11)]
 Five_el=  num[:5]
+print(f'Original list: {num}')
 print(f'Extracted first five elements: {Five_el}')
 print(f'Reversed extracted elements: {Five_el[::-1]}')
